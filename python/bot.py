d008a33f84b7b41c35ced64dec65fab1ad9b3787
Fooping <fukuhala@gmail.com>
2023-03-19 00:24:19 +0900

    http://アラート追加

--- python/bot.py
+++ python/bot.py
@@ -85,6 +85,7 @@ async def on_message(message):
         await message.channel.send(file=discord.File(filepath))
         await message.channel.send(m)
     if message.content.startswith('http://'):
-        m =  "↑このURL大丈夫なんだギャオ？クリックする前に確認したほうがいいギャオ！！"
+        await message.delete()#メッセージの削除
+        m =  message.author.name + "さんの貼ってくれたURL怪しいのでごめんけど、消したギャオ！"
         await message.channel.send(m)
 client.run(config.DISCORD_TOKEN)
